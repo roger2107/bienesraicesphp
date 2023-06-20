f48439a82b38c0c2efe825dbe5cdc18dd513d81e
roger2107 <rogero.dev@gmail.com>
2023-06-20 12:50:37 -0500

    lLectura de propiedades desde ls BD Lista

--- admin/index.php
+++ admin/index.php
@@ -1,19 +1,61 @@
 <?php
+
+    //Importar la conexion
+    require '../includes/config/database.php';
+    $db = conectarDB();
+
+    //Escribir el query
+    $query = "SELECT * FROM propiedades";
+
+    //Consultar la BD (Ejecutar el query)
+    $resultadoConsulta = mysqli_query($db, $query);
+
+    //Muestra mensaje condicional
     $resultado = $_GET['resultado']??null;
     //echo $_GET['resultado'];
     require '../includes/funciones.php';
-    
+    //Incluye un template
     incluirTemplate('header');
 ?>
 
     <main class="contenedor seccion">
-        <h1>Admisntrador de BienesRaices</h1>
+        <h1>Administrador de BienesRaices</h1>  
         <?php if(intval($resultado) === 1): ?>
             <p class="alerta exito">Anuncio creado correctamente</p>
         <?php endif ?>
         <a href="/bienesraices/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
-    </main>
 
+        <table class="propiedades">
+            <thead>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </thead>
+            <tbody> <!-- Mostrar los resulados -->
+
+                <?php  while($propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
+                <tr>
+                    <td> <?php echo $propiedad['id']; ?> </td>
+                    <td> <?php echo $propiedad['titulo']; ?> </td>
+                    <td><img src="/bienesraices/imagenes/<?php echo $propiedad['imagen']; ?>" alt="imagen casa" class="imagen-tabla"></td>   
+                    <td>$<?php echo $propiedad['precio']; ?></td>
+                    <td> 
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endwhile; ?>
+            </tbody>
+    </table>
+    </main>
+    
+    
 <?php
+
+    //Cerrar la conexion 
+    mysqli_close($db);
+    
     incluirTemplate('footer');
 ?>
